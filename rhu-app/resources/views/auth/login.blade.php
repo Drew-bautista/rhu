4b7b63e086101504190bae366aff2251bf20bc03
Drew-bautista <142853218+Drew-bautista@users.noreply.github.com>
2025-10-19 01:07:15 +0800

    newwwssss

--- rhu-app/resources/views/auth/login.blade.php
+++ rhu-app/resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Rural Health Unit - Login</title>
     <link rel="icon" type="image/png" href="{{ asset('img/medic_logo.png') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
